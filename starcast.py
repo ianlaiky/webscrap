02eeeb92a4f3e5730ad025e9a63be24a02aa39b6
Ian <152772a@mymail.nyp.edu.sg>
2017-07-04 22:28:21 +0800

    add support for starcast

--- starcast.py
+++ starcast.py
@@ -12,26 +12,27 @@ soup = BeautifulSoup(data, "lxml")
 
 print(soup)
 
-
+increment =0
 
 for link in soup.find_all('img'):
 
     image = link.get("src")
     imag = image.replace("image", "original")
     # print(imag.find("http"))
-    if(imag.find("http://cfile")==0):
+    if(imag.find("http://mimgnews1.naver.net")==0):
         print(imag)
 
-        imgSplit=os.path.split(imag)
-        if (imgSplit[1][-3:]!="gif"):
-            imgNam = imgSplit[1]+".jpg"
-        else:
-            imgNam = imgSplit[1]
-        r2=requests.get(imag,headers=headers)
-        # print(imgNam)
-        with open(imgNam,"wb") as f:
-            f.write(r2.content)
+        idnexofQ = imag.index('?')
+        imag = imag[:idnexofQ]
+        print("imagi"+imag)
 
+        imgSplit = os.path.split(imag)
+
+        imgNam = imgSplit[1]
+        r2 = requests.get(imag, headers=headers)
+        # print(imgNam)
+        with open(imgNam, "wb") as f:
+            f.write(r2.content)
 
 
 
